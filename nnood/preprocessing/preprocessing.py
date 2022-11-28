e00e4a3d4542afee41d053cbab4eebc3c96f3a1f
tuhinmallick <tuhin.mllk@gmail.com>
2022-11-28 16:40:08 +0100

    Added heraus preprocessing

--- nnood/preprocessing/preprocessing.py
+++ nnood/preprocessing/preprocessing.py
@@ -5,6 +5,7 @@ from typing import Tuple, Union
 
 import SimpleITK as sitk
 import numpy as np
+import cv2
 from scipy.ndimage.interpolation import map_coordinates
 from skimage.transform import resize
 
@@ -296,3 +297,65 @@ class GenericPreprocessor:
                                                                  force_separate_z)
 
         return resampled_data.astype(np.float32), properties
+
+    def letterbox(
+    im,
+    new_shape=(640, 640),
+    color=(114, 114, 114),
+    auto=False,
+    scaleFill=False,
+    scaleup=True,
+    stride=32,):
+        # Resize and pad image while meeting stride-multiple constraints
+        shape = im.shape[:2]  # current shape [height, width]
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio (new / old)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:  # only scale down, do not scale up (for better val mAP)
+            r = min(r, 1.0)
+
+        # Compute padding
+        ratio = r, r  # width, height ratios
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+        if auto:  # minimum rectangle
+            dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+        elif scaleFill:  # stretch
+            dw, dh = 0.0, 0.0
+            new_unpad = (new_shape[1], new_shape[0])
+            ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+        dw /= 2  # divide padding into 2 sides
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        im = cv2.copyMakeBorder(
+            im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+        )  # add border
+        return im, ratio, (dw, dh)
+
+
+    def find_roi(image):
+        gradient = 5
+        img = image.copy()
+        if len(img.shape) == 3:
+            img = img[:, :, 0]
+        upper_part = img[:60, :]
+        medians = np.median(upper_part, axis=0)
+
+        first_part = medians[gradient:]
+        second_part = medians[:-gradient]
+        differences = first_part - second_part
+
+        first_element = np.argmin(differences[: int(0.66 * img.shape[1])])
+        starting_point = np.argmax(differences[first_element : first_element + 300])
+        ending_point = np.argmin(differences[first_element + starting_point + 200 :])
+
+        starting_col = first_element + starting_point - 2 * gradient
+        ending_col = first_element + starting_point + ending_point + 200 + 4 * gradient
+        return starting_col, ending_col
